8ea4e2cf7900e308c53b479b768e5218fce34c07
Moses <asugam9@gmail.com>
2024-11-13 21:24:54 +0300

    read

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+// 
+
+
 
 const http = require('http')
 const port = 3000
 const fs =require('fs');
+const { error } = require('console');
 const server = http.createServer(function(req,res){
-    fs.readFile('')
-}
-)
- server.listen(port,function(err){
+    fs.readFile('index.html',(err,data) =>{   
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write(data);
+        return res.end();        
+    });
+})
+server.listen(port,function(err){
     if(err){
         console.log('no',err)
     }
