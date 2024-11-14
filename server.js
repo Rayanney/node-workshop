8b72f233f417ad6994d6fdd8cfeda189d7bf4edd
Moses <asugam9@gmail.com>
2024-11-14 21:26:38 +0300

    node write

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const port = 3001;
 const fs = require('fs');
 
+// Read File
+
 const server = http.createServer(function (req, res) {
     fs.readFile('note.txt', (err, data) => {
         if (err) {
@@ -16,6 +18,36 @@ const server = http.createServer(function (req, res) {
         }
     });
 });
+
+// Create File
+const data = "Hello Flurry Derivsh";
+
+fs.writeFile('Newfile.txt', data, (err) =>{
+    if(err){
+        console.error('Error creating file:', err);
+    }else{
+        console.log("file has been created!!");
+    }
+
+});
+//Update File
+
+fs.appendFile("Newfile1.txt"," Flurry Devrish GO my COde", (err) =>{
+    if(err){
+        console.error('Error creating file:', err);
+    }else{
+        console.log("file has been updated!!");
+    }
+})
+
+//Delete File
+// fs.unlink('Newfile.txt',(err) =>{
+//     if(err){
+//         console.error("FIle not deleted", err);        
+//     }else{
+//         console.log("File Deleted Succesfully!!!");
+//     }
+// });
 
 server.listen(port, function (err) {
     if (err) {
